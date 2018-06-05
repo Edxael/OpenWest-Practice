7fe99e51c932e5f153f63c667853532df256f2d6
Edxael <zerphyr.r@gmail.com>
2018-06-04 23:13:52 -0600

    update

---
+++ 02-ss/server.js
@@ -0,0 +1,41 @@
+// ===[ Dependencies ]=========================================
+const express = require('express')
+const ejs = require('ejs')
+const app = express()
+
+
+
+// ===[ Middleware ]===========================================
+app.set('view engine', 'ejs')
+app.use(express.static(__dirname + '/views'))
+
+
+
+// ===[ Routes ]===============================================
+app.get('/', (req, res) => {
+    res.render('pages/00-Home');
+})
+
+app.get('/1', (req, res) => {
+    res.render('pages/01-ayumi');
+})
+
+app.get('/2', (req, res) => {
+    res.render('pages/02-madc');
+})
+
+app.get('/3', (req, res) => {
+    res.render('pages/03-blackp');
+})
+
+app.get('/4', (req, res) => {
+    res.render('pages/04-taeyang');
+})
+
+
+
+// ===[ Server Listener ]======================================
+app.listen(3000, (err) => {
+    if(err){ throw err }
+    console.log("Server Listeinig on port: 300 ........")
+})
